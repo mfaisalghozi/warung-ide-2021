b6ea651284c277c8b2648a0f3663a495374f10ac
i-faisal.ghozi <i-faisal.ghozi@dana.id>
2021-09-16 19:52:46 +0700

    feat: test ask pass or not

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -2,6 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h1>Hi i'm here to test the git</h1>
+    <h2>Another changing to check if it ask for pass</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
