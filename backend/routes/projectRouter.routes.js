0aa01ec453aa6d95f1520b2d384b5c8bf90e36a6
i-faisal.ghozi <i-faisal.ghozi@dana.id>
2021-10-22 22:42:24 +0700

    feat: CRUD Sample for mySQL dummy

--- backend/routes/projectRouter.routes.js
+++ backend/routes/projectRouter.routes.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req,res) => {
+router.get('/projectRouter', (req,res) => {
     res.json({
         "foo": "bar",
     })
 })
 
-router.get('/hello', (req,res) => {
+router.get('/projectRouter/hello', (req,res) => {
     res.json({
         "Hello": "World",
     })
