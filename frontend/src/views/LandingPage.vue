058a018c9859950ad1679270aa54684329750c67
i-faisal.ghozi <i-faisal.ghozi@dana.id>
2021-10-24 15:01:53 +0700

    feat: initate first landing page

--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -1,38 +1,17 @@
-<template>  
-  <div>
-      <h1>THIS IS LANDING PAGE</h1>
-      <h2>Checking state : {{ this.number }}</h2>
-      <button @click="addNumber">
-        add numb
-      </button>
-      <button @click="subtractNumber">
-        subtract numb
-      </button>
+<template>
+  <div class="landingPageWrapper">
+    <h1>Hai this is landing cool</h1>
   </div>
 </template>
 
 <script>
-import store from '../store'
-import { mapState } from 'vuex' 
- 
 export default {
   name: 'LandingPage',
-  computed: {
-    ...mapState({
-      number: state => store.state.number
-    })
-  },
-  methods: {
-    addNumber () {
-      store.dispatch('addingNumber')
-    },
-    subtractNumber () {
-      store.dispatch('subtractNumber')
-    }
-  }
 }
 </script>
 
-<style>
-
-</style>
+<style lang="less" scoped>
+.landingPageWrapper {
+  background-color: pink;
+}
+</style>>
