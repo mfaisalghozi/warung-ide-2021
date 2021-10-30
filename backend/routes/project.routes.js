5574b726f68a836d6f547160e090329d3b0848ba
bpc096 <58715789+bpc096@users.noreply.github.com>
2021-10-30 15:51:52 +0700

    belom jadi be nya

--- backend/routes/project.routes.js
+++ backend/routes/project.routes.js
@@ -1,17 +1,29 @@
 const express = require('express')
 const router = express.router()
-// TODO: Add projectController for handling project API
-// const projectController
 
+// TODO: Add projectController for handling project API
+
+// const projectController
+const Project = require("../controllers/project.controller.js");
 
 // Create new project idea
+router.get("/new", (req, res) => {
+    res.render('new');
+});
+
+router.post("/new", Project.create);
 
 // Get all project idea 
+router.get("/project", Project.findAll);
 
 // Get project idea based on projectId
+router.get("/project/:projectId", Project.findOne);
 
 // Get project idea based on categoryId
+router.get("/project/:categoryId", Project.findOne);
 
 // Update project idea
+router.put("/project/:projectId", Project.update);
 
 // Delete project idea
+router.delete("/project/:projectId", Project.delete);
